8e14a3d3ae27514c2e357e01d7dafe88b30687c1
camward <camward@yandex.ru>
2022-11-22 20:24:42 +0400

    навигация

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import ProjectOne from "./projects/ProjectOne";
+import ProjectTwo from "./projects/ProjectTwo";
+import ProjectThree from "./projects/ProjectThree";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <div className="app_header">
+          <Header />
+        </div>
+        <div className="app_body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="one" element={<ProjectOne />} />
+            <Route path="two" element={<ProjectTwo />} />
+            <Route path="three" element={<ProjectThree />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </div>
+      </div>{" "}
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
